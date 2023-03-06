f71ee01fd0466325c3ab5faebc1403da6f6c36d2
Denis.Karev <goofy.adun@gmail.com>
2023-03-06 14:14:47 +0300

    new gnviews

---
+++ python/gntsviews/commandline.py
@@ -0,0 +1,37 @@
+from models.gview import gview
+from datetime import datetime
+
+class commandline():
+    sortoption: bool     # if implenemt sor option ))
+    edit_usage: str
+
+    def __init__(self) -> None:
+        self.sortoption = None
+        self.edit_usage = '-e usage string'
+        # self.empty = f'Nothing to {} -- base is empty'
+
+    def edit_usage(self):
+        print(self.edit_usage)
+        print(self.help_txt)
+
+    def err_print(self, text):
+        print(text)
+
+
+    def gnote_short_print(self, n: dict):
+        idx = n['index']
+        tit = n['title']
+        cmdate = datetime.fromtimestamp(n['cmdate']).strftime("%d/%m/%Y %H:%M:%S")
+        return f'{idx}\t{tit}\t\t{cmdate}'
+    
+    def gnote_full(self, n: dict) -> str:
+        idx = n['index']
+        tit = n['title']
+        text = n['notetext']
+        cmdate = datetime.fromtimestamp(n['cmdate']).strftime("%d/%m/%Y %H:%M:%S")
+        return f'{idx}\t{tit}\t\t{text}\t{cmdate}'
+
+    def gnote_print(self, message: str, gn: dict, t: str):
+        if t in 'sf':
+            if t == 'f': print(f'{message} {self.gnote_full(gn)}')
+            else:  print(f'{message} {self.gnote_short_print(gn)}')
